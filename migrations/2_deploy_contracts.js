f95d1047709cbc3ae996162df28540ecf23f86e7
stephen antoni <stephen@garuda.to>
2024-09-19 09:18:13 +0700

    created deploy script

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,22 @@
+const ForwarderFactory = artifacts.require("ForwarderFactory");
+const Forwarder = artifacts.require("Forwarder");
+
+module.exports = async function(deployer, network, accounts) {
+  // Deploy ForwarderFactory
+  await deployer.deploy(ForwarderFactory);
+  const forwarderFactory = await ForwarderFactory.deployed();
+
+  console.log("ForwarderFactory deployed to:", forwarderFactory.address);
+
+  // Deploy a sample Forwarder using the factory
+  // Replace this address with your desired pool address
+  const samplePoolAddress = "0x8270ef105315729d0A12E340e54E3F1930E56054";
+  
+  const result = await forwarderFactory.createForwarder(samplePoolAddress);
+  
+  // Get the ForwarderCreated event
+  const forwarderCreatedEvent = result.logs.find(log => log.event === "ForwarderCreated");
+  const forwarderAddress = forwarderCreatedEvent.args.forwarderAddress;
+
+  console.log("Sample Forwarder deployed to:", forwarderAddress);
+};
